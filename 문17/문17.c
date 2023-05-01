08912a735e90af68b0e04fafd96ad4fad1bfd2a6
martin-yeo <sangsooyeo@gmail.com>
2023-05-01 10:53:00 +0900

    문17번 입력 오류 체크 루틴 추가

--- 문17/문17.c
+++ 문17/문17.c
@@ -6,8 +6,22 @@ int main(void) {
 		printf("반복횟수를 입력하시오(1~26) [0은 종료]: ");
 		int checkScan = scanf_s("%d", &cnt);
 		
+		//문자 입력 오류 체크
+		if (checkScan < 1) {
+			printf("입력오류입니다. 숫자를 입력하세요\n");
+			char flushStr[256];
+			scanf_s("%s", flushStr, (unsigned) sizeof(flushStr));
+			continue;
+		}
+
 		// 종료조건 체크
 		if (cnt == 0) break;
+
+		// 숫자 입력 범위 체크
+		if (cnt < 1 || cnt > 26) {
+			printf("1~26 사이의 숫자를 입력하세요.\n");
+			continue;
+		}
 
 		for (int i = 0; i < cnt; i++) {
 			for (int k = cnt - 1; k > i; k--) {
